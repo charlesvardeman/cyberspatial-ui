25f3c24f672d47586e335f4ef857654bba707c31
Caleb Reinking <creinkin@nd.edu>
2018-03-12 17:18:38 -0400

    Making user maps start at municipality assigned by user keywords

--- njcoast/views.py
+++ njcoast/views.py
@@ -2,13 +2,22 @@ from django.http import JsonResponse
 from guardian.shortcuts import get_objects_for_user
 from django.conf import settings
 from geonode.layers.models import Layer
+from django.views.generic import TemplateView
 
+'''
+  This function is used to respond to ajax requests for which layers should be
+  visible for a given user. Borrowed a lot of this from the GeoNode base code
+  for the layer list view page that is provided by Geonode
+'''
 def my_gis_layers(request):
 
+    # get all object (django-guardian) that the user is allowed to see
     permitted_ids = get_objects_for_user(
         request.user,
         'base.view_resourcebase').values('id')
 
+    # filter it down to layers so that we know that they will have the right
+    # object properties
     permitted_layers = Layer.objects.filter(id__in=permitted_ids)
 
     layers_dictionary = {"layers":[]}
@@ -28,36 +37,28 @@ def my_gis_layers(request):
 
 
     return JsonResponse(layers_dictionary)
-        
-#    return JsonResponse({
-#  "layers": [
-#    {
-#      "id": "natural2015",
-#      "name": "Natural Imagery 2015",
-#      "group": "Base Mapping",
-#      "layer_link": "http://192.168.33.10:8080/geoserver/wms?",
-#      "layer": "geonode:Natural2015"
-#    },
-#    {
-#      "id": "flowdirection",
-#      "name": "Flow Direction",
-#      "group": "Base Mapping",
-#      "layer_link": "https://services.nationalmap.gov/arcgis/services/nhd/MapServer/WMSServer?",
-#      "layer": "9"
-#    },
-#    {
-#      "id": "waterbody",
-#      "name": "Waterbody - Small Scale",
-#      "group": "Base Mapping",
-#      "layer_link": "https://services.nationalmap.gov/arcgis/services/nhd/MapServer/WMSServer?",
-#      "layer": "8"
-#    },
-#    {
-#      "id": "line",
-#      "name": "Line - Small Scale",
-#      "group": "Base Mapping",
-#      "layer_link": "https://services.nationalmap.gov/arcgis/services/nhd/MapServer/WMSServer?",
-#      "layer": "2"
-#    }
-#  ]
-#    })
+
+'''
+  Template View that injects some parameters about starting map position based on user keywords
+  This should be reworked at some point to be more elegantly tied to user's Groups
+  and values controlled by GeoNode
+'''
+class MapTemplateView(TemplateView):
+    template_name = 'map.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(MapTemplateView, self).get_context_data(**kwargs)
+
+        keywords = self.request.user.keywords
+
+        # no keywords assigned OR both Keansburg + Berkeley will start the user
+        # at Keansburg, as well as the obvious case of just having keansburg
+        if keywords.filter(name="keansburg").exists() or len(keywords.all()) == 0:
+            context['home_latitude'] = "40.4417743"
+            context['home_longitude'] = "-74.1298643"
+            context['zoom_level'] = 14
+        elif keywords.filter(name="berkeley").exists():
+            context['home_latitude'] = "39.9051846"
+            context['home_longitude'] = "-74.1808381"
+            context['zoom_level'] = 13
+        return context
