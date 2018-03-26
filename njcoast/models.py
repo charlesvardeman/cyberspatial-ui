1188c6f22e59099e4839b4ac8664926bdae87dde
Caleb Reinking <creinkin@nd.edu>
2018-03-26 15:35:29 -0400

    add owner to annotation object

--- njcoast/models.py
+++ njcoast/models.py
@@ -23,10 +23,12 @@ class NJCMapAnnotation(models.Model):
     text = models.CharField(max_length=250, blank=True, null=True)
     data = models.TextField()
     map = models.ForeignKey(NJCMap, on_delete=models.CASCADE)
-    # owner = models.ForeignKey(
-    #     settings.AUTH_USER_MODEL,
-    #     on_delete=models.CASCADE,
-    # )
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return "%s - %s - %s" % (self.map, self.type, self.text)
