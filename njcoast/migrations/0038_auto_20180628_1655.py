29beca03aa2fbb62c6877955c1ff657b2a7c167c
Chris Sweet <csweet1@nd.edu>
2018-06-28 17:56:17 -0400

    Added textfields for request and approve additional munis.

---
+++ njcoast/migrations/0038_auto_20180628_1655.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('njcoast', '0037_njcmap_modified'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='njcusermeta',
+            name='additional_muni_approved',
+            field=models.TextField(null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='njcusermeta',
+            name='additional_muni_request',
+            field=models.TextField(null=True, blank=True),
+        ),
+    ]
