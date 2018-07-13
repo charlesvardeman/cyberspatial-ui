0845115e012663b1f48dddd87ff0d8ec8e2041ce
James Sweet <james.sweet88@googlemail.com>
2018-07-13 13:14:08 -0400

    Removed Sea Level Rise option from active storms. Fixes S.12

--- njcoast/templates/js/storm_visualization.js
+++ njcoast/templates/js/storm_visualization.js
@@ -92,13 +92,6 @@ function add_active_storm_to_menu(active_storms) {
             "<div class=\"well plain orange scenarios\">\n" +
             "                  <h5>Explore Scenarios</h5>\n" +
             "                  <ul class=\"map-layers\">\n" +
-            "                    <li class=\"explore-scenario-controls\">\n" +
-            "                      <p>Sea Level Rise</p>\n" +
-            "                      <div class=\"shp-controls\">\n" +
-            "                        <input id=\"" + camelcaseName + "_slr_slider\" class=\"range-input sealevel " + camelcaseName + "_slr_range\" data-target=\"slr\" min=\"0\" max=\"4.5\" step=\"1.5\" value=\"0\" type=\"range\">\n" +
-            "                        <input id=\"" + camelcaseName + "_input_slr\" class=\"sealevel num-input job-input " + camelcaseName + "_slr_input\" name=\"slr\" value=\"0\" type=\"text\"> ft\n" +
-            "                      </div>\n" +
-            "                    </li>\n" +
             "                    <li class=\"explore-scenario-controls controls-" + camelcaseName + "\">\n" +
             "                      <p>Coastal Protection (e.g. dunes)</p>\n" +
             "                      <ul class=\"beta-feature-not-available one-line " + camelcaseName + "_dunes_radios\">\n" +
